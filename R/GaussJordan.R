3d2288014d70eddfe7766bc4ef68a5bb0549fdfd
Amine TEFFAL <a.teffal@gmail.com>
2015-09-18 13:28:08 +0000

    Update GaussJordan.R

--- R/GaussJordan.R
+++ R/GaussJordan.R
@@ -15,20 +15,23 @@ GaussJordan <- function (A1, A2=matrix())
 
   if(N>1) {
 
-    for(i in 1:(N-1)) {
+    for(i in 1:(N)) {
       #Choix du plus grand pivot
       l <- i
       m <- abs(A1[i,i])
-      for(j in (i+1):N) {
-        s <- abs(A1[j,i])
-        if(m<s) {
-          l <- j
-          m <- s
+
+      for(j in i:N) {
+        if(j!=i) {
+          s <- abs(A1[j,i])
+          if(m<s) {
+            l <- j
+            m <- s
+          }
         }
       }
       if(l!=i) {
         #Echange des lignes i et l de A1
-        for(j in i:N) {
+        for(j in 1:N) {
           temp <- A1[i,j]
           A1[i,j] <- A1[l,j]
           A1[l,j] <- temp
@@ -46,7 +49,7 @@ GaussJordan <- function (A1, A2=matrix())
 
       p <- 1/A1[i,i]
       #Division de la ligne par le pivot
-      for(j in i:N) {
+      for(j in 1:N) {
         A1[i,j] <- p*A1[i,j]
       }
 
@@ -55,28 +58,28 @@ GaussJordan <- function (A1, A2=matrix())
       }
 
       #Génération des zéros
-      for(k in (i+1):N) {
-        for(j in (i+1):N) {
-          A1[k,j] <- A1[k,j] - A1[k,i]*A1[i,j]
-        }
-        cat("k=",k,"\n")
-        cat("j=",j,"\n")
-        cat("i=",i,"\n")
+      for(k in 1:N) {
+        if(k!=i) {
+          temp2 <- A1[k,i]
+          for(j in 1:N) {
+            if(j!=i) {
+              A1[k,j] <- A1[k,j] - temp2*A1[i,j]
+            }
+          }
 
-        for(j in 1:M) {
-          cat("M=",M,"\n")
-          A2[k,j] <- A2[k,j] - A1[k,i]*A2[i,j]
+          for(j in 1:M) {
+            A2[k,j] <- A2[k,j] - temp2*A2[i,j]
+          }
+          A1[k,i] <- 0
         }
-
-        A1[k,i] <- 0
       }
     }
 
-    p <- 1/A1[N,N]
-    A1[N,N] <- p*A1[N,N]
-    for(j in 1:M) {
-      A2[N,j] <- p*A2[N,j]
-    }
+#     p <- 1/A1[N,N]
+#     A1[N,N] <- p*A1[N,N]
+#     for(j in 1:M) {
+#       A2[N,j] <- p*A2[N,j]
+#     }
 
 
     return(list("A1"=A1,"A2"=A2))
