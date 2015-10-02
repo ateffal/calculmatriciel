d96a15e2441289c2de6d1f794a274a3c0187c819
Amine TEFFAL <a.teffal@gmail.com>
2015-10-02 10:41:46 +0000

    Update InverseMat.R
    Test that M is a matrix.

--- R/InverseMat.R
+++ R/InverseMat.R
@@ -1,6 +1,6 @@
 InverseMat <- function (M)
 {
-
+  if(class(M)!="matrix") return("M n'est pas une matrice")
   if(nrow(M)!=ncol(M)) return("La matrice M doit être carrée")
   return(GaussJordan(M,diag(nrow(M) ))$A2)
 
